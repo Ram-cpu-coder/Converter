5b586eb3f251f3aadf4ebb4e63917055fa968e1b
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-12-15 16:21:12 +1100

    entry List rendering with the input data

--- src/InputNotToDoList.jsx
+++ src/InputNotToDoList.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-const InputNotToDoList = () => {
+const InputNotToDoList = ({ setTaskInput, setHourInput, addTask }) => {
+  const handleInputTask = (e) => {
+    setTaskInput(e.target.value);
+  };
+  const handleInputHours = (e) => {
+    setHourInput(parseFloat(e.target.value));
+  };
+
   return (
     <div className="flex justify-center items-center p-5 gap-2 drop-shadow-lg">
       <div className="">
@@ -9,6 +16,7 @@ const InputNotToDoList = () => {
           className="form-control"
           id="task"
           placeholder="Task"
+          onChange={handleInputTask}
         />
       </div>
       <div className="">
@@ -17,13 +25,14 @@ const InputNotToDoList = () => {
           className="form-control"
           id="hour"
           placeholder="Hours"
+          onChange={handleInputHours}
         />
       </div>
       <button
         type="button"
         id="addTask"
         className="btn btn-primary"
-        onclick="addTask()"
+        onClick={addTask}
       >
         Add New Task
       </button>
--- src/NotToDoList.jsx
+++ src/NotToDoList.jsx
@@ -1,15 +1,49 @@
 import React from "react";
+import { useState } from "react";
 
 import InputNotToDoList from "./InputNotToDoList";
 import EntryLists from "./EntryLists";
 import NotToDoListResult from "./NotToDoListResult";
 
 const NotToDoList = () => {
+  const randomIdGenerator = () => {
+    let randomId = "";
+    const randomStringLength = 10;
+    const string =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    for (let i = 0; i < randomStringLength; i++) {
+      let randomIdindex = Math.floor(Math.random() * string.length);
+      randomId += string[randomIdindex];
+    }
+    return randomId;
+  };
+
+  const [TaskInput, setTaskInput] = useState("Task");
+  const [HourInput, setHourInput] = useState("Hours");
+  const [showEntryList, setShowEntryList] = useState(false);
+  const [taskList, setTaskList] = useState([]);
+
+  const addTask = () => {
+    const itemTaskList = {
+      id: randomIdGenerator(),
+      task: { TaskInput },
+      hour: { HourInput },
+      type: "entry",
+    };
+    setTaskList([...taskList, itemTaskList]);
+    setShowEntryList(true);
+    console.log("CLicked");
+  };
+  console.log(taskList);
   return (
     <div>
       <h1 className="text-center mb-4">Not To Do List</h1>
-      <InputNotToDoList />
-      <EntryLists />
+      <InputNotToDoList
+        setTaskInput={setTaskInput}
+        setHourInput={setHourInput}
+        addTask={addTask}
+      />
+      <EntryLists taskList={taskList} showEntryList={showEntryList} />
       <NotToDoListResult />
     </div>
   );
