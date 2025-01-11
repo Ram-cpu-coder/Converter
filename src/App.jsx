340b0f5b2e76f8fda1fd397cfa584f5e2b36b9b5
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-01-11 13:32:26 +1100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import TemperatureConverter from "./TemperatureConverter";
 import Calculator from "./Calculator";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div>
