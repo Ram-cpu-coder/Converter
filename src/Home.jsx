7dea9ad59a240b5bf467448d1f9c777c80ba10e1
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-01-12 12:30:04 +1100

    s

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
     <div className="flex flex-col justify-center items-center h-[90vh] text-center">
       <h1 className="text-5xl p-5 font-bold">Welcome To Converters</h1>
       <h2 className="text-2xl italic">
-        Head to <span className="text-sky-500 px-2">Services</span> for list of
-        converters
+        Head to <span className="text-sky-500 px-2">Converters</span> for list
+        of converters
       </h2>
     </div>
   );
